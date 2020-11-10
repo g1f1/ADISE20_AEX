0871af63bab0d16d7de675e9c5b434d05ad52807
elenhka98 <elenhka98@gmail.com>
2020-11-10 21:33:21 +0200

    index

--- index.php
+++ index.php
@@ -1,24 +1,57 @@
-﻿<!DOCTYPE html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-	<title>LOGIN</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+<title>Backgammon game </title>
+<script src="jquery.min.js" charset="utf-8"></script>
+
+<link rel="stylesheet" type="text/css" href="styles.css">
+
+<link rel="icon" href="g.png">
+
 </head>
+
 <body>
 
-    <img src="g.png" width="500" height="300">
-	<form action="login.php" method="post">
-		<h2>LOGIN</h2>
-		<?php if(isset($_GET['error'])) { ?>
+
+ <div class="loginbox">
+    <img src="g.png" class="auto-style1">
+    
+        <form method="post" action="login.php">
+        
+        <?php if(isset($_GET['error'])) { ?>
 			<p class="error"><?php echo $_GET['error']; ?></p>
 		<?php } ?>
-		<label>User Name</label>
-		<input type="test" name="uname" placeholder="User Name">
-		<br>
-		<label>Password</label>
-		<input type="password" name="password" placeholder="Password">
-		
-		<button type="submit">LOGIN</button>
-	 </form>
+
+            <div class="txtb">
+          <input type="text" name="uname">
+          <span data-placeholder="Username" ></span>
+        </div>
+
+        <div class="txtb">
+          <input type="password"  name="password">
+          <span data-placeholder="Password" ></span>
+        </div>
+             <input type="submit" name="" value="Login">
+            <a href="">Forgot your password?</a><br> <a href="register.php">Don't have an account?</a>
+        </form>
+        
+    </div>
+    
+    
+      <script type="text/javascript">
+      $(".txtb input").on("focus",function(){
+        $(this).addClass("focus");
+      });
+
+      $(".txtb input").on("blur",function(){
+        if($(this).val() == "")
+        $(this).removeClass("focus");
+      });
+
+      </script>
+
 </body>
+
 </html>
