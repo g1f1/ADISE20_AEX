049fa94894809376755fb07fd4f7624a3bc4e66f
elenhka98 <elenhka98@gmail.com>
2020-11-11 13:05:17 +0200

    sign up page

--- register.php
+++ register.php
@@ -1,54 +1,42 @@
-
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-
+<!DOCTYPE html>
+<html>
 
 <head>
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-<title>Backgammon game </title>
-<script src="jquery.min.js" charset="utf-8"></script>
-
-
-
-<link rel="stylesheet" type="text/css" href="register.css">
-<link rel="icon" href="g.png">
+	<title>SIGN UP</title>
+	<link rel="stylesheet" type="text/css" href="../../../../../xampp/htdocs/ADISE/style.css">
+	<link rel="icon" href="../../../../../xampp/htdocs/ADISE/g.png">
 
 </head>
-
 <body>
 
+    <img src="../../../../../xampp/htdocs/ADISE/g.png" width="600" height="210">
+	
 
- <div class="loginbox">
-    <img src="g.png" class="auto-style1">
-    
-        <form method="post" action="signup-check.php">
+      <form method="post" action="signup-check.php">
         
          <?php if (isset($_GET['error'])) { ?>
      		<p class="error"><?php echo $_GET['error']; ?></p>
      	<?php } ?>
-     	
-     	<div class="txtb">
-        <?php if (isset($_GET['email'])) { ?>
-               <input type="text" 
+     	<label>Email</label>
+      <?php if (isset($_GET['email'])) { ?>
+               <input type="email" 
                       name="name" 
-                      placeholder="Email"
+                      placeholder="Type Email..."
                       value="<?php echo $_GET['email']; ?>">      
          <?php }else{ ?>
-               <input type="text" 
-                      name="email" 
+               <input type="email" 
+                      name="email"
+                      placeholder="Type Email..."
                       >          
                       
                       <?php }?>
-          <span data-placeholder="Email"></span>
           <?php if (isset($email_error)): ?>
       	<span><?php echo $email_error; ?></span>
       <?php endif ?>
-        </div>
+       
 
            
-		     <div class="txtb">
+		    <label>User Name</label>
           <?php if (isset($_GET['uname'])) { ?>
                <input type="text" 
                       name="uname" 
@@ -57,48 +45,32 @@
           <?php }else{ ?>
                <input type="text" 
                       name="uname" 
+                      placeholder="Type Username..."
                     >         
                      <?php }?>
-                    <span data-placeholder="Username"></span>
                      <?php if (isset($name_error)): ?>
 	  			<span><?php echo $name_error; ?></span>
 			  <?php endif ?>
-        </div>
-
-        <div class="txtb">
-          <input type="password" 
-                 name="password" 
-                 > 
-                 <span data-placeholder="Password"></span>
-        </div>
-        <div class="txtb">
-         <input type="password" 
-                 name="re_password" 
-                 >
-                 <span data-placeholder="Re_Password"></span>
-
-        </div>
-
-	             <input type="submit" name="reg_user" value="Sign in">
-	             
-	              <a href="index.php" class="ca">Already have an account?</a>
-                   </form>
         
-    </div>
-    
-    
-      <script type="text/javascript">
-      $(".txtb input").on("focus",function(){
-        $(this).addClass("focus");
-      });
 
-      $(".txtb input").on("blur",function(){
-        if($(this).val() == "")
-        $(this).removeClass("focus");
-      });
+        <label>Password</label>
+         <input type="password" 
+         		placeholder="Type Password..."
+                 name="password"> 
+                     
+			<label>Re-type Password</label>
+           <input type="password" 
+           		placeholder="Re-Type Password..."
+                 name="re_password">
+                 
+           
+	      <input type="submit" name="reg_user" class="log" value="Sign in">
 
-      </script>
 
+
+		<a href="index.php">Already a member?Login in</a>
+
+		
+</form>
 </body>
-
 </html>
