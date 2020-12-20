023c17763b1bf2ee35932f3c48dabed3f162dfbe
a4bit enas <a4bit@hotmail.com>
2020-12-20 18:25:11 +0200

    ...

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php include ('lib/db_conn.php')?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>LOGIN</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="icon" href="imgs/g.png">
+
+
+</head>
+<body>
+
+    <img src="imgs/g.png" width="600" height="210">
+	<form action="login.php" method="post">
+		<h2>LOGIN</h2>
+		<?php if(isset($_GET['error'])) { ?>
+			<p class="error"><?php echo $_GET['error']; ?></p>
+		<?php } ?>
+		<label>User Name</label>
+		<input type="test" name="username" placeholder="Type Username...">
+
+		<label>Password</label>
+		<input type="password" name="password" placeholder="Type Password...">
+
+		<input type="submit" value="LOGIN" name="login_user" class="log">
+		
+		<a href="register.php">Not a member?<b>  Sign up</a>
+
+		
+</form>
+</body>
+</html>
