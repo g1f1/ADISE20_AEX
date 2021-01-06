0290196e70f3f7387d103a9a178e4ae5eeb2a720
ΕΛΕΝΗ ΚΑΤΣΕΠΑ <73292015+elenhka98@users.noreply.github.com>
2021-01-06 17:20:17 +0200

    Update sign-up.php

--- sign-up.php
+++ sign-up.php
@@ -4,10 +4,10 @@ session_start();
 include ('lib/db_conn.php');
 
 
+
 $username = "";
 $email    = "";
 $errors = array(); 
-
 
 
 if (isset($_POST['reg_user'])) {
@@ -15,16 +15,6 @@ if (isset($_POST['reg_user'])) {
   $email = mysqli_real_escape_string($conn, $_POST['email']);
   $password_1 = mysqli_real_escape_string($conn, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($conn, $_POST['password_2']);
-
-$db = mysqli_connect('localhost', 'root', '', 'demo');
-
-
-if (isset($_POST['reg_user'])) {
-    $username = mysqli_real_escape_string($db, $_POST['username']);
-  $email = mysqli_real_escape_string($db, $_POST['email']);
-  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
-  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
-
 
 
   if (empty($username)) {header("Location: register.php?error=Username is required");
@@ -39,11 +29,7 @@ if (isset($_POST['reg_user'])) {
   }
  
   $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
-
   $result = mysqli_query($conn, $user_check_query);
-
-  $result = mysqli_query($db, $user_check_query);
-
   $user = mysqli_fetch_assoc($result);
   
   if ($user) {     if ($user['username'] === $username) {
@@ -61,7 +47,6 @@ else
 
 else{
 
-
 		
     $password = md5($password_1);
   	$query = "INSERT INTO users (username, email, password) 
@@ -73,16 +58,5 @@ else{
   	header("Location: home.php");
   
 }}
-
-
-    $password = md5($password_1);
-  	$query = "INSERT INTO users (username, email, password) 
-  			  VALUES('$username', '$email', '$password')";
-  	mysqli_query($db, $query);
-  	$_SESSION['username'] = $username;
-  	$_SESSION['success'] = "You are now logged in";
-  	header("Location: home.php");
-  
-}
 
 
