4eba613c91bae06222c54095bacb0321bacd0626
ΕΛΕΝΗ ΚΑΤΣΕΠΑ <73292015+elenhka98@users.noreply.github.com>
2021-01-06 15:00:22 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -43,44 +43,6 @@ function loadDoc() {
 
 =======
 ﻿
-function rules() {
-  var modal = document.getElementById("rules");
-
-
-var btn = document.getElementById("back");
-
-
-var span = document.getElementsByClassName("close")[0];
-
-
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-}
-
-function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("info").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "About.php", true);
-  xhttp.send();
-}
 
 
 
