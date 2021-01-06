a1a51bd3d48f2029beb565216640a40a4fae5508
ΕΛΕΝΗ ΚΑΤΣΕΠΑ <73292015+elenhka98@users.noreply.github.com>
2021-01-06 17:10:03 +0200

    Update sign-up.php

--- sign-up.php
+++ sign-up.php
@@ -9,66 +9,7 @@ $email    = "";
 $errors = array(); 
 
 
-if (isset($_POST['reg_user'])) {
-  $username = mysqli_real_escape_string($conn, $_POST['username']);
-  $email = mysqli_real_escape_string($conn, $_POST['email']);
-  $password_1 = mysqli_real_escape_string($conn, $_POST['password_1']);
-  $password_2 = mysqli_real_escape_string($conn, $_POST['password_2']);
 
-
-  if (empty($username)) {header("Location: register.php?error=Username is required");
-      exit();
-	 }
-  if (empty($email)) { header("Location: register.php?error=Email is required"); exit();
- }
-  if (empty($password_1)) { header("Location: register.php?error=Password is required");exit();
- }
-  if ($password_1 != $password_2) {
-	header("Location: register.php?error=The two passwords do not match"); exit();
-  }
- 
-  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
-  $result = mysqli_query($conn, $user_check_query);
-  $user = mysqli_fetch_assoc($result);
-  
-  if ($user) {     if ($user['username'] === $username) {
-      header("Location: register.php?error=Username already exists");
-      exit();
-
-    }
-else 
-    if ($user['email'] === $email) {
-     header("Location: register.php?error=Email already exists");
-     exit();
-
-    }
-  }
-
-else{
-
-		
-    $password = md5($password_1);
-  	$query = "INSERT INTO users (username, email, password) 
-  			  VALUES('$username', '$email', '$password')";
-   	mysqli_query($conn, $query);
-  	
-  	$_SESSION['username'] = $username;
-  	$_SESSION['success'] = "You are now logged in";
-  	header("Location: home.php");
-  
-}}
-
-<?php
-session_start();
-include ('lib/db_conn.php');
-
-
-$username = "";
-$email    = "";
-$errors = array(); 
-
-
-<<<<<<< HEAD
 if (isset($_POST['reg_user'])) {
   $username = mysqli_real_escape_string($conn, $_POST['username']);
   $email = mysqli_real_escape_string($conn, $_POST['email']);
@@ -83,7 +24,7 @@ if (isset($_POST['reg_user'])) {
   $email = mysqli_real_escape_string($db, $_POST['email']);
   $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
->>>>>>> d17fbc539843cce6a3343fb50d96ecd1af331779
+
 
 
   if (empty($username)) {header("Location: register.php?error=Username is required");
@@ -102,7 +43,7 @@ if (isset($_POST['reg_user'])) {
   $result = mysqli_query($conn, $user_check_query);
 =======
   $result = mysqli_query($db, $user_check_query);
->>>>>>> d17fbc539843cce6a3343fb50d96ecd1af331779
+
   $user = mysqli_fetch_assoc($result);
   
   if ($user) {     if ($user['username'] === $username) {
@@ -119,7 +60,7 @@ else
   }
 
 else{
-<<<<<<< HEAD
+
 
 		
     $password = md5($password_1);
@@ -144,6 +85,4 @@ else{
   
 }}
 
->>>>>>>> d17fbc539843cce6a3343fb50d96ecd1af331779:server.php
->>>>>>> d17fbc539843cce6a3343fb50d96ecd1af331779
-// ... 
+
